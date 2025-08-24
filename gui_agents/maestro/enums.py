56473cdbad50d25e2d67a99e255110dd12105563
hgf <haoguangfu@deepmatrix.com.cn>
2025-08-24 15:43:39 +0800

    fix(TRIGGER_CODE_BY_MODULE)

--- gui_agents/maestro/enums.py
+++ gui_agents/maestro/enums.py
@@ -183,104 +183,73 @@ class TriggerCode(str, Enum):
     ERROR_RECOVERY = "error_recovery" # unknown -> INIT
     
 # 按模块分类的触发代码字典
-TRIGGER_CODE_BY_MODULE = {
-    "manager_replan": [
-        # 任务状态规则相关
-        "rule_replan_long_execution",       # controller -> manager: replan
-        
-        # 状态处理相关 - INIT状态
-        "no_subtasks", # INIT -> manager: replan
-        "init_error", # INIT -> manager: replan
-        
-        # 状态处理相关 - GET_ACTION状态
-        "work_cannot_execute", # worker: get_action -> manager: replan
-        "no_worker_decision", # worker: get_action -> manager: replan
-        "get_action_error", # worker: get_action -> manager: replan
-        
-        # 状态处理相关 - QUALITY_CHECK状态
-        "quality_check_failed", # evaluator: quality_check -> manager: replan
-        "quality_check_error", # evaluator: quality_check -> manager: replan
-        
-        # 状态处理相关 - SUPPLEMENT状态
-        "supplement_completed", # manager: supplement -> manager: replan
-        "supplement_error", # manager: supplement -> manager: replan
-        
-        # 状态处理相关 - FINAL_CHECK状态
-        "final_check_failed", # evaluator: final_check -> manager: replan
-    ],
+class TRIGGER_CODE_BY_MODULE:
+    # Manager replan 相关的触发代码
+    MANAGER_REPLAN_CODES = {
+        "work_cannot_execute": TriggerCode.WORK_CANNOT_EXECUTE.value,
+        "quality_check_failed": TriggerCode.QUALITY_CHECK_FAILED.value,
+        "no_worker_decision": TriggerCode.NO_WORKER_DECISION.value,
+        "get_action_error": TriggerCode.GET_ACTION_ERROR.value,
+        "quality_check_error": TriggerCode.QUALITY_CHECK_ERROR.value,
+        "final_check_failed": TriggerCode.FINAL_CHECK_FAILED.value,
+        "rule_replan_long_execution": TriggerCode.RULE_REPLAN_LONG_EXECUTION.value,
+        "no_subtasks": TriggerCode.NO_SUBTASKS.value,
+        "init_error": TriggerCode.INIT_ERROR.value,
+        "supplement_completed": TriggerCode.SUPPLEMENT_COMPLETED.value,
+        "supplement_error": TriggerCode.SUPPLEMENT_ERROR.value,
+    }
     
-    "manager_supplement": [
-        # 状态处理相关 - GET_ACTION状态
-        "worker_supplement", # worker: get_action -> manager: supplement
-        
-        # 状态处理相关 - QUALITY_CHECK状态
-        "quality_check_supplement", # evaluator: quality_check -> manager: supplement
-    ],
+    # Manager supplement 相关的触发代码
+    MANAGER_SUPPLEMENT_CODES = {
+        "worker_supplement": TriggerCode.WORKER_SUPPLEMENT.value,
+        "quality_check_supplement": TriggerCode.QUALITY_CHECK_SUPPLEMENT.value,
+    }
     
-    "worker_get_action": [
-        # 状态处理相关 - INIT状态
-        "subtask_ready", # INIT -> worker: get_action
-        
-        # 状态处理相关 - EXECUTE_ACTION状态
-        "execution_error", # executor: execute_action -> worker: get_action
-        "command_completed", # executor: execute_action -> worker: get_action
-        "no_command", # executor: execute_action -> worker: get_action
-        
-        # 状态处理相关 - QUALITY_CHECK状态
-        "quality_check_passed", # evaluator: quality_check -> worker: get_action
-        
-        # 状态处理相关 - PLAN状态
-        "subtask_ready_after_plan", # manager: replan -> worker: get_action
-        
-        # 状态处理相关 - FINAL_CHECK状态
-        "final_check_pending", # evaluator: final_check -> worker: get_action
-    ],
+    # Worker get_action 相关的触发代码
+    WORKER_GET_ACTION_CODES = {
+        "subtask_ready": TriggerCode.SUBTASK_READY.value,
+        "execution_error": TriggerCode.EXECUTION_ERROR.value,
+        "command_completed": TriggerCode.COMMAND_COMPLETED.value,
+        "no_command": TriggerCode.NO_COMMAND.value,
+        "quality_check_passed": TriggerCode.QUALITY_CHECK_PASSED.value,
+        "subtask_ready_after_plan": TriggerCode.SUBTASK_READY_AFTER_PLAN.value,
+        "final_check_pending": TriggerCode.FINAL_CHECK_PENDING.value,
+    }
     
-    "evaluator_quality_check": [
-        # 任务状态规则相关
-        "rule_quality_check_steps", # controller -> evaluator: quality_check
-        "rule_quality_check_repeated_actions",  # controller -> evaluator: quality_check
-        
-        # 状态处理相关 - GET_ACTION状态
-        "worker_success", # worker: get_action -> evaluator: quality_check
-        "worker_stale_progress", # worker: get_action -> evaluator: quality_check
-    ],
+    # Evaluator quality_check 相关的触发代码
+    EVALUATOR_QUALITY_CHECK_CODES = {
+        "rule_quality_check_steps": TriggerCode.RULE_QUALITY_CHECK_STEPS.value,
+        "rule_quality_check_repeated_actions": TriggerCode.RULE_QUALITY_CHECK_REPEATED_ACTIONS.value,
+        "worker_success": TriggerCode.WORKER_SUCCESS.value,
+        "worker_stale_progress": TriggerCode.WORKER_STALE_PROGRESS.value,
+    }
     
-    "evaluator_final_check": [
-        # 状态处理相关 - QUALITY_CHECK状态
-        "all_subtasks_completed", # evaluator: quality_check -> evaluator: final_check
-    ],
+    # Evaluator final_check 相关的触发代码
+    EVALUATOR_FINAL_CHECK_CODES = {
+        "all_subtasks_completed": TriggerCode.ALL_SUBTASKS_COMPLETED.value,
+    }
     
-    "executor_execute_action": [
-        # 状态处理相关 - GET_ACTION状态
-        "worker_generate_action", # worker: get_action -> executor: execute_action
-        
-        # 状态处理相关 - QUALITY_CHECK状态
-        "quality_check_execute_action", # evaluator: quality_check -> executor: execute_action
-    ],
+    # Executor execute_action 相关的触发代码
+    EXECUTOR_EXECUTE_ACTION_CODES = {
+        "worker_generate_action": TriggerCode.WORKER_GENERATE_ACTION.value,
+        "quality_check_execute_action": TriggerCode.QUALITY_CHECK_EXECUTE_ACTION.value,
+    }
     
-    "INIT": [
-        # 状态处理相关 - GET_ACTION状态
-        "no_current_subtask_id", # worker: get_action | executor: execute_action | evaluator: quality_check -> INIT
-        "subtask_not_found", # worker: get_action | executor: execute_action -> INIT
-        
-        # 状态处理相关 - PLAN状态
-        "plan_error", # manager: replan -> INIT
-        
-        # 错误恢复相关
-        "unknown_state", # unknown -> INIT
-        "error_recovery", # unknown -> INIT
-    ],
+    # INIT 状态相关的触发代码
+    INIT_STATE_CODES = {
+        "no_current_subtask_id": TriggerCode.NO_CURRENT_SUBTASK_ID.value,
+        "subtask_not_found": TriggerCode.SUBTASK_NOT_FOUND.value,
+        "plan_error": TriggerCode.PLAN_ERROR.value,
+        "unknown_state": TriggerCode.UNKNOWN_STATE.value,
+        "error_recovery": TriggerCode.ERROR_RECOVERY.value,
+    }
     
-    "DONE": [
-        # 任务状态规则相关
-        "rule_max_state_switches_reached",  # controller -> controller: done
-        "rule_plan_number_exceeded",              # controller -> controller: done
-        "rule_state_switch_count_exceeded",  # controller -> controller: done
-        "rule_task_completed",                          # controller -> controller: done
-        
-        # 状态处理相关 - FINAL_CHECK状态
-        "final_check_error", # evaluator: final_check -> END: Done
-        "final_check_passed", # evaluator: final_check -> END: Done
-    ]
-} 
+    # DONE 状态相关的触发代码
+    DONE_STATE_CODES = {
+        "rule_max_state_switches_reached": TriggerCode.RULE_MAX_STATE_SWITCHES_REACHED.value,
+        "rule_plan_number_exceeded": TriggerCode.RULE_PLAN_NUMBER_EXCEEDED.value,
+        "rule_state_switch_count_exceeded": TriggerCode.RULE_STATE_SWITCH_COUNT_EXCEEDED.value,
+        "rule_task_completed": TriggerCode.RULE_TASK_COMPLETED.value,
+        "final_check_error": TriggerCode.FINAL_CHECK_ERROR.value,
+        "final_check_passed": TriggerCode.FINAL_CHECK_PASSED.value,
+    }
